a4da1d609def8f80e6c6945efe1ce79099b8a903
Michael Levy <malevy@ucdavis.edu>
2017-05-10 16:59:39 -0700

    HMPL will calculate on directed network

--- R/HMPL.R
+++ R/HMPL.R
@@ -1,6 +1,7 @@
 #' Harmonic mean path length
 #'
 #' @param net network object. if `net` is directed, will be made undirected by the weak rule (if i->j or j->i then i-j).
+#' @param makeUndirected Should directed network be made undirected by weak rule? Default is FALSE
 #'
 #' @return Harmonic mean of path lengths between dyads.
 #' @references Newman MEJ, 2003. The structure and function of complex networks. SIAM Review.
@@ -12,15 +13,19 @@
 #' @examples
 #' n <- makeNetwork(10, .1)
 #' HMPL(n)
+#' n <- makeNetwork(10, .1, directed = TRUE)
+#' HMPL(n)
+HMPL <- function(net, makeUndirected = FALSE) {
 
-HMPL <- function(net) {
-
-  if (is.directed(net)) {
-    warning(deparse(substitute(net)), " is directed. HMPL only work for undirected networks. Converting... ")
-    net <- symmetrize(net[,], rule = "weak")
+  if (makeUndirected) {
+    if (!is.directed(net)) {
+      stop("You said to make ", deparse(substitute(net)), " undirected, but it already is.")
+    } else {
+      net <- symmetrize(net[,], rule = "weak")
+    }
   }
 
-  d = geodist(net)[["gdist"]]
-  return(mean(d[upper.tri(d)]^-1)^-1)
-
+  d = sna::geodist(net)[["gdist"]]
+  d = d[upper.tri(d) | lower.tri(d)]
+  return(mean(d^-1)^-1)
 }
